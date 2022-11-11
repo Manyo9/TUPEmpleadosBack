a24c5a6db02a4fd3755d8f74e4ab2bea1f60d0da
Manyo <agusmangini@hotmail.com>
2022-11-11 16:57:30 -0300

    arreglo inserts con numeros fijos en vez de porcen

--- db/DML.sql
+++ db/DML.sql
@@ -17,32 +17,32 @@ VALUES ('Sofía','Marquez', '1995-07-23', '2021-08-06', 100600.00, 'Sistemas'); 
 ### EMPLEADO 1
 INSERT INTO recibos 
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (1, 1, 2022, 110000.00, 10.00, 5.00, 7.50, 3.50);	 # 1
+VALUES (1, 1, 2022, 110000, 11000, 5500.50, 18250.34, 3850.53);	 # 1
 INSERT INTO recibos 
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (1, 2, 2022, 113000.00, 10.00, 5.00, 7.50, 3.50);	 # 2
+VALUES (1, 2, 2022, 113000.00, 11300, 5678.36, 18336.01, 3985.36);	 # 2
 INSERT INTO recibos 
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (1, 3, 2022, 116300.00, 10.00, 5.00, 7.50, 3.50);	 # 3
+VALUES (1, 3, 2022, 116300.00, 11630, 5852.22, 18450.33, 4036.44);	 # 3
 
 ### Empleado 2
 INSERT INTO recibos
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (2, 1, 2022, 90000.00, 8.00, 4.90, 8.50, 3.20);	 # 1
+VALUES (2, 1, 2022, 90000.00, 8000.00, 4500.15, 12580.33, 1350.48);	 # 1
 INSERT INTO recibos 
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (2, 2, 2022, 91000.00, 8.50, 5.00, 8.50, 3.20);	 # 2
+VALUES (2, 2, 2022, 91000.00, 8100.25, 4555.55, 12601.10, 1360.53);	 # 2
 INSERT INTO recibos 
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (2, 3, 2022, 93300.00, 8.50, 5.00, 8.50, 3.20);	 # 3
+VALUES (2, 3, 2022, 93300.00, 8233.33, 2623.03, 12650.00, 1401.69);	 # 3
 
 ### Empleado 3
 INSERT INTO recibos
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (3, 1, 2022, 145120.00, 15.00, 8.00, 10.00, 5.25);	 # 1
+VALUES (3, 1, 2022, 145120.00, 20100.50, 9112.23, 16300.00, 2223.56);	 # 1
 INSERT INTO recibos 
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (3, 2, 2022, 146200.00, 15.50, 8.00, 10.00, 5.50);	 # 2
+VALUES (3, 2, 2022, 146200.00, 20320.36, 9162.60, 16325.00, 2250.36);	 # 2
 INSERT INTO recibos 
 (legajo_empleado, mes, anio, sueldo_bruto, incremento_antiguedad, ded_jubilacion, ded_obra_social, ded_fondo_alta_compl)
-VALUES (3, 3, 2022, 147350.00, 15.50, 8.10, 10.50, 6.00);	 # 3
+VALUES (3, 3, 2022, 147350.00, 21003.30, 9210.01, 16402.96, 2320.74);	 # 3
